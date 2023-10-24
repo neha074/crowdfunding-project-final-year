076f853fe4680075f670e786166a6d7e2deef766
neha074 <nehamanickam74@gmail.com>
2023-10-24 21:39:54 +0530

    update

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -42,6 +42,26 @@ export const StateContextProvider = ({ children }) => {
     }
   };
 
+  const getCampaigns =async()=>
+  {
+    const campaigns = await contract.call("getCampaigns");
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+
+    return parsedCampaings;
+  }
+
   return (
     <StateContext.Provider
       value={{
@@ -49,6 +69,7 @@ export const StateContextProvider = ({ children }) => {
         contract,
         connect,
         createCampaign: publishCampaign,
+        getCampaigns,
       }}
     >
       {children}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import { StateContextProvider } from "./context";
-// import {Mumbai} from "@thirdweb-dev/chains";
+import {Mumbai} from "@thirdweb-dev/chains";
 
 import App from "./App";
 import "./index.css";
@@ -12,9 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <ThirdwebProvider
-    clientId="86270acbbec0d81c0e4cd663e1f51e0"
-    desiredChainId="80001"
-    // activeChain={Mumbai}
+    clientId="e068724341fefe49e6ae73dc7a6400e4"
+    activeChain={Mumbai}
   >
     <Router>
       <StateContextProvider>
